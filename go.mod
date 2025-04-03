9a7c7b0f61984d02a7f5308d7e5658bb0a6bfc0d
Starballoon <zhaoxiangqianty@126.com>
2025-04-03 13:39:14 +0800

    Add Some Command Line Args Support And Ignore Case Searching

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Starballoon/shachar
 
-go 1.24.2
+go 1.24
+
+require github.com/spf13/pflag v1.0.6
--- tool/cmd/searchcomp/main.go
+++ tool/cmd/searchcomp/main.go
@@ -3,20 +3,64 @@ package main
 import (
 	"archive/zip"
 	"bytes"
+	"errors"
 	"fmt"
+	"github.com/spf13/pflag"
 	"io"
 	"os"
 	"strings"
 )
 
-// main Example: go run tool\cmd\searchcomp\main.go "C:\envs\maven\repository\org\apache\poi\ooxml-schemas\1.3\ooxml-schemas-1.3.jar" Manifest
-func main() {
-	if len(os.Args) < 3 || len(strings.TrimSpace(os.Args[1])) == 0 || len(strings.TrimSpace(os.Args[2])) == 0 {
-		fmt.Println("Invalid")
-		return
+var (
+	Help         bool
+	Recursive    bool
+	IgnoreCase   bool
+	ArchiveTypes []string
+	SearchPath   string
+	SearchString string
+)
+var (
+	SupportedArchiveTypes = []string{
+		"jar",
+		//"war",
+		"zip",
+		//"ear",
+		//"rar",
+		//"tar",
+		//"gz",
+		//"tgz",
+		//"bz2",
+		//"tbz",
+		//"xz",
+		//"txz",
+		//"7z",
+		//"deb",
+		//"rpm",
 	}
-	input := os.Args[1]
-	keyword := []byte(os.Args[2])
+	SupportedArchiveTypesStr = strings.Join(SupportedArchiveTypes, ", ")
+)
+
+func init() {
+	pflag.BoolVarP(&Help, "help", "h", false, "Help")
+	pflag.BoolVarP(&Recursive, "recursive", "R", false, "Recursive")
+	pflag.BoolVarP(&IgnoreCase, "ignore-case", "I", false, "Ignore Case")
+	pflag.StringSliceVarP(&ArchiveTypes, "type", "t", SupportedArchiveTypes,
+		"Archive Types to Search, Default All, Supported Types: "+SupportedArchiveTypesStr)
+	pflag.Parse()
+	if Help {
+		pflag.Usage()
+		os.Exit(0)
+	}
+	if err := ValidateInputs(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+// main Example: go run tool\cmd\searchcomp\main.go Manifest "C:\envs\maven\repository\org\apache\poi\ooxml-schemas\1.3\ooxml-schemas-1.3.jar"
+func main() {
+	input := SearchPath
+	keyword := []byte(SearchString)
 	if !IsFileExists(input) {
 		fmt.Println("Invalid")
 		return
@@ -39,7 +83,9 @@ func main() {
 				fmt.Println("Failed")
 				return
 			}
-			if bytes.Contains(bs, keyword) {
+			if !IgnoreCase && bytes.Contains(bs, keyword) {
+				fmt.Println(f.Name)
+			} else if IgnoreCase && bytes.Contains(bytes.ToLower(bs), bytes.ToLower(keyword)) {
 				fmt.Println(f.Name)
 			}
 			err = rc.Close()
@@ -54,4 +100,27 @@ func main() {
 func IsFileExists(f string) bool {
 	_, err := os.Stat(f)
 	return err == nil
+}
+
+func ValidateInputs() error {
+	args := pflag.Args()
+	if len(args) != 2 {
+		switch len(args) {
+		case 0:
+			return errors.New("Missing Search Pattern or Path")
+		case 1:
+			return errors.New("Missing Search Pattern and Path")
+		default:
+			return errors.New("Too Much Inputs")
+		}
+	}
+	SearchString = args[0]
+	if len(strings.TrimSpace(SearchString)) == 0 {
+		return fmt.Errorf("Invalida Search Pattern: %s", SearchString)
+	}
+	SearchPath = args[1]
+	if len(strings.TrimSpace(SearchPath)) == 0 {
+		return fmt.Errorf("Invalida Search Path: %s", SearchPath)
+	}
+	return nil
 }
