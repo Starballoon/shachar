18047d667f232712f1e3fffc1b8e944dc1f08d96
Starballoon <30925057+Starballoon@users.noreply.github.com>
2026-04-28 13:19:01 +0800

    Try DS Solution

--- _worker.js
+++ _worker.js
@@ -1,9 +1,23 @@
-const TARGET = "https://coding.dashscope.aliyuncs.com";
+// const TARGET = "https://coding.dashscope.aliyuncs.com";
+const TARGET = 'https://ark.cn-beijing.volces.com/api/coding/v3';
+
+// export default {
+//   async fetch(request, env, ctx) {
+//     const url = new URL(request.url);
+//     const newUrl = TARGET + url.pathname + url.search;
+//     return fetch(newUrl, request);
+//   }
+// }
 
 export default {
   async fetch(request, env, ctx) {
     const url = new URL(request.url);
     const newUrl = TARGET + url.pathname + url.search;
-    return fetch(newUrl, request);
+    const proxyRequest = new Request(newUrl, {
+      method: request.method,
+      headers: request.headers,
+      body: request.body,
+    });
+    return fetch(proxyRequest);
   }
-}
+};
